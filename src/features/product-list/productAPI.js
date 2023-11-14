feaab1b56e3380a2b13bd0dd15f84b9fc2ccc449
harshraj6582 <harshraj211430@gmail.com>
2023-11-14 11:04:27 +0530

    Part  5 Commit

--- src/features/product-list/productAPI.js
+++ src/features/product-list/productAPI.js
@@ -1,7 +1,8 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
+export function fetchAllProducts() {
   return new Promise(async (resolve) => {
-     const response = await fetch('https://localhost:8080')
+    // TODO - We will not hard-code 
+     const response = await fetch('https://localhost:8080/products')
      const data = await response.json()
      resolve({data})
   }
